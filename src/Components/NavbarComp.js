ea713c07cfdd053e79a16e1ebbf7e693436f3813
William Bogans <trebogans@gmail.com>
2018-10-16 17:43:57 -0700

    Need to finish this navbar!!!!!

--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -6,27 +6,33 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className="a navBar">
+        <label htmlFor="hamburger-menu" className="ham-sym Navbar-tog">
+          &#9776;
+        </label>
+        <input type="checkbox" id="hamburger-menu" />
         <ul className="side-links">
-          <li>
-            <a href="#Header">Main</a>
-          </li>
-          <li>
-            <a href="#Portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="#TechTools">Tech Tools</a>
-          </li>
-          <li>
-            <a href="#About">About</a>
-          </li>
-          <li>
-            <a href="#Contact">Contact</a>
-          </li>
-          <li>
-            <a href="#TODO:send this to the blog sub domain">Blog</a>
-          </li>
+          <div className="Navbar-items">
+            <li>
+              <a href="#Header">Main</a>
+            </li>
+            <li>
+              <a href="#Portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="#TechTools">Tech Tools</a>
+            </li>
+            <li>
+              <a href="#About">About</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact</a>
+            </li>
+            <li>
+              <a href="#TODO:send this to the blog sub domain">Blog</a>
+            </li>
+            <SocialLinks />
+          </div>
         </ul>
-        <SocialLinks />
       </div>
     );
   }
