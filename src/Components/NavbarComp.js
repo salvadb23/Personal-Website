928e708e538000cfef4f3890aea03cb014b05476
William Bogans <trebogans@gmail.com>
2018-10-21 20:45:46 -0700

    closer to hamburger nav target

--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -6,32 +6,30 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className="a navBar">
-        <label htmlFor="hamburger-menu" className="ham-sym Navbar-tog">
+        <label htmlFor="hamburger-menu" className="ham-sym">
           &#9776;
         </label>
         <input type="checkbox" id="hamburger-menu" />
-        <ul className="side-links nav-toggle">
-          <div className="Navbar-items">
-            <li>
-              <a href="#Header">Main</a>
-            </li>
-            <li>
-              <a href="#Portfolio">Portfolio</a>
-            </li>
-            <li>
-              <a href="#TechTools">Tech Tools</a>
-            </li>
-            <li>
-              <a href="#About">About</a>
-            </li>
-            <li>
-              <a href="#Contact">Contact</a>
-            </li>
-            <li>
-              <a href="#TODO:send this to the blog sub domain">Blog</a>
-            </li>
-            <SocialLinks />
+        <ul className="side-links">
+          <div className="Navbar-link">
+            <a href="#Header">Main</a>
           </div>
+          <div className="Navbar-link">
+            <a href="#Portfolio">Portfolio</a>
+          </div>
+          <div className="Navbar-link">
+            <a href="#TechTools">Tech Tools</a>
+          </div>
+          <div className="Navbar-link">
+            <a href="#About">About</a>
+          </div>
+          <div className="Navbar-link">
+            <a href="#Contact">Contact</a>
+          </div>
+          <div className="Navbar-link">
+            <a href="#TODO:send this to the blog sub domain">Blog</a>
+          </div>
+          <SocialLinks />
         </ul>
       </div>
     );
