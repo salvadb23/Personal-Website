8b21c40040e46811b62927f5207c32918545d847
William Bogans <trebogans@gmail.com>
2018-10-22 00:58:01 -0700

    HAMBURGER NAV WORKS

--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -6,32 +6,43 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className="a navBar">
-        <label htmlFor="hamburger-menu" className="ham-sym">
+        <label
+          htmlFor="hamburger-menu"
+          className="ham-sym"
+          onClick={this.burgerToggle}
+        >
           &#9776;
         </label>
         <input type="checkbox" id="hamburger-menu" />
-        <ul className="side-links">
-          <div className="Navbar-link">
+        <ul className="side-links nav-toggle">
+          <div>
             <a href="#Header">Main</a>
           </div>
-          <div className="Navbar-link">
+          <div>
             <a href="#Portfolio">Portfolio</a>
           </div>
-          <div className="Navbar-link">
+          <div>
             <a href="#TechTools">Tech Tools</a>
           </div>
-          <div className="Navbar-link">
+          <div>
             <a href="#About">About</a>
           </div>
-          <div className="Navbar-link">
+          <div>
             <a href="#Contact">Contact</a>
           </div>
-          <div className="Navbar-link">
-            <a href="#TODO:send this to the blog sub domain">Blog</a>
+          <div>
+            {/* #TODO:send this to the blog sub domain
+              <a href="#">Blog</a> */}
           </div>
           <SocialLinks />
         </ul>
       </div>
     );
+  }
+  burgerToggle() {
+    let nav = document.querySelector(".navBar");
+    nav.style.height === "10%"
+      ? (nav.style.height = "100%")
+      : (nav.style.height = "10%");
   }
 }
